98161d9b20f203a0d4f54a9d859b8edfb4aeee79
0daxx <0daxx@proton.me>
2026-06-20 23:27:48 +0530

    added badge Icon

--- projects/p2_CoffeeVilla/src/navigator/HomeTabNavigator.tsx
+++ projects/p2_CoffeeVilla/src/navigator/HomeTabNavigator.tsx
@@ -1,48 +1,62 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/tabs/HomeScreen';
-import DineScreen from '../screens/tabs/DineScreen';
-import ProfileScreen from '../screens/tabs/ProfileScreen';
-
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/tabs/HomeScreen";
+import DineScreen from "../screens/tabs/DineScreen";
+import ProfileScreen from "../screens/tabs/ProfileScreen";
+import OrderScreen from "../screens/tabs/OrderScreen";
+import SearchScreen from "../screens/SearchScreen";
 
 const Tab = createBottomTabNavigator();
-// const Tab = createBottomTabNavigator();
+
 const TABS = [
   { name: "Home", component: HomeScreen, icon: "home-outline" },
-  { name: "Dine", component: DineScreen, icon: "restaurant-outline" },
-  // { name: "Grocery", component: GroceryScreen, icon: "cart-outline" },
-  // { name: "Event", component: EventScreen, icon: "calendar" },
-  { name: "Profile", component: ProfileScreen, icon: "person" },
+
+  { name: "Search", component: SearchScreen, icon: "search-outline" },
+
+  { name: "Order", component: OrderScreen, icon: "cart" },
+  { name: "My Profile", component: ProfileScreen, icon: "person" },
 ];
 
-// import { Ionicons } from "@react-native-vector-icons/ionicons/static";
 import { Ionicons } from "@expo/vector-icons";
 
-export default function HomeTabNavigator() {
-  return (
-    <Tab.Navigator
-      // headershown={false}
-      screenOptions={{ headerShown: false }}
-    >
-      {TABS.map((tab) => (
-        <Tab.Screen
-          key={tab.name}
-          name={tab.name}
-          component={tab.component}
-          options={{
-            tabBarIcon: () => (
-              <Ionicons name={tab.icon} size={24} color="black" />
-            ),
-          }}
-        />
-      ))}
-    </Tab.Navigator>
+import { CartContext } from "../context/CartProvider";
 
-    // <Tab.Navigator>
-    //   <Tab.Screen name="Home" component={HomeScreen} />
-    //   <Tab.Screen name="Dine" component={DineScreen} />
-    //   <Tab.Screen name="Grocery" component={GroceryScreen} />
-    //   <Tab.Screen name="Profile" component={ProfileScreen} />
-    // </Tab.Navigator>
+export default function HomeTabNavigator() {
+  const { cart } = React.useContext(CartContext);
+  const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
+  return (
+
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarIcon: () => <Ionicons name={"home-outline"} size={24} color="black" /> }} />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Ionicons name={"search-outline"} size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Order"
+        component={OrderScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => <Ionicons name={"cart"} size={24} color="black" />,
+          tabBarBadge: cartItemCount ? cartItemCount : undefined,
+        }}
+      />
+      <Tab.Screen
+        name="My Profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Ionicons name={"person"} size={24} color="black" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
