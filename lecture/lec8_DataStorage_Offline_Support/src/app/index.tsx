13b22d7ade6db1b952cd11132a51c3dc6e50f438
0daxx <0daxx@proton.me>
2026-07-07 22:47:29 +0530

    Secure Storage

--- lecture/lec8_DataStorage_Offline_Support/src/app/index.tsx
+++ lecture/lec8_DataStorage_Offline_Support/src/app/index.tsx
@@ -2,59 +2,94 @@ import { Text, View, StyleSheet } from "react-native";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
-import {useState} from "react";
-export default function Index() {
+import { useState } from "react";
+import * as SecureStore from "expo-secure-store";
+
+export function Index1() {
   const [data, setData] = useState<string | null>(null);
 
-  // set item 
-  const setItems = async ( key : string ,  value: string) => {
+  // set item
+  const setItems = async (key: string, value: string) => {
     try {
-      await AsyncStorage.setItem(key, value)
+      await AsyncStorage.setItem(key, value);
     } catch (e) {
       // saving error
     }
-  }
-  const getItems = async (key : string) => {
+  };
+  const getItems = async (key: string) => {
     try {
-      const value = await AsyncStorage.getItem(key)
-      if(value !== null) {
-        setData(value)
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        setData(value);
       }
-    } catch(e) {
+    } catch (e) {
       // error reading value
     }
-  }
+  };
 
-  const removeItem = async (key : string ) => {
+  const removeItem = async (key: string) => {
     try {
-      const value = await AsyncStorage.removeItem(key)
-    } catch (error) {
-      
-    }
-  }
+      const value = await AsyncStorage.removeItem(key);
+    } catch (error) {}
+  };
 
-  const clearAll = async () =>{
+  const clearAll = async () => {
     try {
-      const value = await AsyncStorage.clear()
-    } catch (error) {
-      
-    }
-  }
+      const value = await AsyncStorage.clear();
+    } catch (error) {}
+  };
 
-  const saveMulti  = async () => {
+  const saveMulti = async () => {
     try {
-      
-    } catch (error) {
-      
-    }
-  }
+    } catch (error) {}
+  };
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    
-    
     </View>
   );
+}
+
+export default function Index() {
+  const [output, setOutput] = useState<string>("");
+
+  const saveTokens = async () => {
+    await SecureStore.setItemAsync("token", "abscds");
+    setOutput("Token saved ");
+  };
+
+  const getToken = async () => {
+    const value = await SecureStore.getItemAsync("token");
+    setOutput(value!);
+  };
+
+  const deleteToken = async () => {
+    await SecureStore.deleteItemAsync("token");
+    setOutput("Token deleted ");
+  };
+
+  const checkAvailablity = async () => {
+    const available = await SecureStore.isAvailableAsync();
+    setOutput(available ? "secureStore available on device" : "not available");
+  };
+
+  // set OBjbect
+  const saveObject = async () => {
+    const user = {
+      name: "code snippet",
+      role: "programmer",
+    };
+    await SecureStore.setItemAsync("user", JSON.stringify(user));
+    setOutput("object saved ")
+  };
+  const getObject = async () => {
+  
+    const userData = await SecureStore.getItemAsync("user");
+    setOutput(userData ? JSON.parse(userData) : "object not found");
+    
+  }; 
+
+  return <View></View>;
 }
 
 const styles = StyleSheet.create({
