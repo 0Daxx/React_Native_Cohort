9aa5fb8975a7454cd8bf94e4efeeeda677d5fa11
0daxx <0daxx@proton.me>
2026-07-12 21:41:03 +0530

    add video capturing

--- lecture/lec_13_camera/src/app/camera.tsx
+++ lecture/lec_13_camera/src/app/camera.tsx
@@ -1,16 +1,25 @@
-import { Button, StyleSheet, Text, View , Image } from "react-native";
-import React from "react";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import React, { useState, useRef } from "react";
 
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 
-import { CameraView, useCameraPermissions } from "expo-camera";
+import {
+  CameraView,
+  useCameraPermissions,
+  useMicrophonePermissions,
+} from "expo-camera";
 const Camera = () => {
   const [permission, requestPermission] = useCameraPermissions();
-  const cameraRef = React.useRef<CameraView>(null);
-  const [ready, setReady] = React.useState<boolean>(false);
+  const cameraRef = useRef<CameraView>(null);
+  const [ready, setReady] = useState<boolean>(false);
 
-  const [photoUri, setPhotoUri] = React.useState<string | null>(null);
+  const [photoUri, setPhotoUri] = useState<string | null>(null);
+
+  const [micPermission, requestMicPermission] = useMicrophonePermissions();
+  const [videoUri, setVideoUri] = useState<string | null>(null);
+
+  const [recording, setRecording] = useState<boolean>(false);
 
   if (!permission) {
     return <ThemedText>Requesting camera permission...</ThemedText>;
@@ -32,6 +41,26 @@ const Camera = () => {
     const photo = await cameraRef.current?.takePictureAsync({ quality: 1 });
     if (photo?.uri) setPhotoUri(photo.uri);
   };
+
+  const startRecording = async () => {
+    if (!micPermission?.granted) {
+      const result = await requestMicPermission();
+      if (!result.granted) {
+        console.log("Microphone permission is not granted");
+        return;
+      }
+    }
+
+    setRecording(true);
+    const video = await cameraRef.current?.recordAsync({ maxDuration: 10 });
+    if (video?.uri) setVideoUri(video?.uri);
+    // video is stored in cache memory 
+    setRecording(false);
+  };
+
+  const stopRecording = () => {
+    cameraRef.current?.stopRecording();
+  };
   return (
     <ThemedView style={{ flex: 1, justifyContent: "center", padding: 24 }}>
       <CameraView
@@ -40,14 +69,25 @@ const Camera = () => {
         onCameraReady={() => setReady(true)}
         facing="back"
         onMountError={(error) => console.log(error)}
+        mode="video" // default is photo
       />
+
+      <Button
+        title={recording ? "stop" : "record"}
+        disabled={!ready}
+        onPress={recording ? stopRecording : startRecording}
+      />
+
       <Button title="Take Photo" onPress={takePhoto} disabled={!ready} />
-      {
-        photoUri && (
-          // <Image source={{  }}  />
-          <Image source={{uri : photoUri}} style={{height : 200}} contentFit="cover"  />
-        )
-      }
+
+      {videoUri && <ThemedText selectable> {videoUri} </ThemedText>}
+      {photoUri && (
+        <Image
+          source={{ uri: photoUri }}
+          style={{ height: 200 }}
+          contentFit="cover"
+        />
+      )}
       <ThemedText style={{ padding: 12 }}>
         {ready ? "Camera is ready" : "Camera is not ready"}
       </ThemedText>
