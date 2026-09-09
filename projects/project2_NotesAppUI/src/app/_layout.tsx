d87895aa2b4be19a39b8c7cf4ad8a01286cc257a
0daxx <0daxx@proton.me>
2026-09-09 15:02:44 +0530

    feat: implement tab navigation with icons for index and notes screens

--- projects/project2_NotesAppUI/src/app/_layout.tsx
+++ projects/project2_NotesAppUI/src/app/_layout.tsx
@@ -1,5 +1,33 @@
+import Ionicons from "@react-native-vector-icons/ionicons";
 import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Tabs />;
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="documents"
+              size={24}
+              color={focused ? "blue" : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notes"
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="document"
+              size={24}
+              color={focused ? "blue" : "gray"}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
