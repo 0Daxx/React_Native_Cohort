7fa74209da16fdcd8c9f8ba6bcdf07cd5a702272
0daxx <0daxx@proton.me>
2026-09-12 17:02:23 +0530

    feat : navigation from notes list to notes screen

--- projects/project2_NotesAppUI/src/app/_layout.tsx
+++ projects/project2_NotesAppUI/src/app/_layout.tsx
@@ -1,37 +1,17 @@
 import Ionicons from "@react-native-vector-icons/ionicons";
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 import {ThemeProvider} from "@/theme/ThemeContext";
 export default function RootLayout() {
   return (
     <ThemeProvider>
-    <Tabs screenOptions={{ headerShown: false ,    }}  >
-      <Tabs.Screen
+    <Stack screenOptions={{ headerShown: false ,    }}  >
+      <Stack.Screen
         name="index"
-        options={{
-          tabBarLabel:"Home",
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="documents"
-              size={24}
-              color={focused ? "rgb(0, 122, 20)" : "gray"}
-              />
-            ),
-          }}
           />
-      <Tabs.Screen
-        name="notes"
-        options={{
-          tabBarLabel:"Note",
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-            name="document"
-            size={24}
-            color={focused ? "rgb(0, 122, 20)" : "gray"}
-            />
-          ),
-        }}
+      <Stack.Screen
+        name="notes/[notes]"
       />
-    </Tabs>
+    </Stack>
     </ThemeProvider>
   );
 }
