2288945899b58e2f0d6eda676bfbde3e3ba4ecd3
0daxx <0daxx@proton.me>
2026-09-18 22:40:54 +0530

    feat: refactor AuthContext to use  zustand persistance with Async Storage

--- projects/project3_Oishii/src/context/AuthContext.tsx
+++ projects/project3_Oishii/src/context/AuthContext.tsx
@@ -1,11 +1,21 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface AuthState {
   isAuth: boolean;
   setIsAuth: (isAuth: boolean) => void;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  isAuth: false,
-  setIsAuth: (isAuth: boolean) => set({ isAuth }),
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      isAuth: false,
+      setIsAuth: (isAuth: boolean) => set({ isAuth }),
+    }),
+    {
+      name: "authStore",
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
