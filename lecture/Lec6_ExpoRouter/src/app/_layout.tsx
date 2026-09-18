6c6fa3b290418bf6ca9ed348c5fbce65a79eae40
0daxx <0daxx@proton.me>
2026-09-18 19:16:34 +0530

    feat: update authentication logic in RootLayout and add RestaurantLayout component

--- lecture/Lec6_ExpoRouter/src/app/_layout.tsx
+++ lecture/Lec6_ExpoRouter/src/app/_layout.tsx
@@ -1,10 +1,10 @@
 import { Tabs, Stack } from "expo-router";
 
 export default function RootLayout() {
-  const isLoggedIn = true;
-  // const isLoggedIn = false;
+  // const isLoggedIn = true;
+  const isLoggedIn = false;
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Protected guard={isLoggedIn}>
         <Stack.Screen name="(tabs)" />
       </Stack.Protected>
