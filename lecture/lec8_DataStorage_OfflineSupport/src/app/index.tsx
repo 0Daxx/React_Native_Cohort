e65f3e65e054a026fa383109bb44c8202b40ab4a
0daxx <0daxx@proton.me>
2026-09-24 22:02:25 +0530

    Added SecureStore functionalities

--- lecture/lec8_DataStorage_OfflineSupport/src/app/index.tsx
+++ lecture/lec8_DataStorage_OfflineSupport/src/app/index.tsx
@@ -1,8 +1,49 @@
 import { Text, View, StyleSheet } from "react-native";
-
+import * as SecureStore from "expo-secure-store";
+import { useState } from "react";
 export default function Index() {
+  const [output, setOutput] = useState<string | null>(null);
+
+  const saveToken = async () => {
+    try {
+      await SecureStore.setItemAsync("token", "ablkihbvv")
+      setOutput("Token Saved")
+    } catch (error) {
+      console.log(error)
+      console.error();
+    }
+  }
+
+  const getToken = async () => {
+    const value = await SecureStore.getItemAsync("token")
+    setOutput(value);
+  }
+
+  const deleteToken = async () => {
+    await SecureStore.deleteItemAsync("token")
+    setOutput("token deleted")
+
+  }
+  const checkAvailability = async () => {
+    const available = await SecureStore.isAvailableAsync()
+    setOutput(
+      available ? "SecureStore available"
+        : "Not available"
+    )
+  }
+  const saveObject = async () => {
+    const user = {
+      name: "Code snipper",
+      role: "Admin"
+    }
+
+    await SecureStore.setItemAsync("user", JSON.stringify(user))
+    setOutput("object saved ")
+  }
   return (
+
     <View style={styles.container}>
+      <Text>{output}</Text>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
