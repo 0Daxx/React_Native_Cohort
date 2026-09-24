9a769ff079d3a5f2bcf22869cec6f36c90c98380
0daxx <0daxx@proton.me>
2026-09-24 22:50:25 +0530

    feat: add initial todo component with basic structure

---
+++ lecture/lec8_DataStorage_OfflineSupport/src/app/todo.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const databse = () => {
+  return (
+    <View>
+      <Text>databse</Text>
+    </View>
+  )
+}
+
+export default databse
+
+const styles = StyleSheet.create({})
